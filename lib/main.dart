2d63e69b397aa311e98f4e8e4f35d25b30b126b9
MD. EMON MIAH <md.emonmiahisthebest@gmail.com>
2023-11-14 00:09:51 +0600

    Fourth Time Push

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:chats_app/All_Binding/all_binding.dart';
 import 'package:chats_app/Views/spalash/spalash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main(){
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   AllBinding().dependencies();
   runApp(const MyApp());
 }
